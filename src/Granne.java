9c6904b0ac27f982d8dfe7cf3e38e524e182d7a2
IsseNisse <Isakhafiu2001>
2019-12-04 15:16:33 +0100

    Read a file with information about the graph

--- src/Granne.java
+++ src/Granne.java
@@ -1,2 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Granne {
+    public static void main(String[] args) {
+
+        int nod = 0;
+        Scanner src = null;
+        try {
+            src  = new Scanner(new File("text.txt"));
+            nod = src.nextInt();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(nod);
+    }
 }
