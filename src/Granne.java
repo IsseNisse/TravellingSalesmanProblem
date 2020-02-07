828d440a15e7396ee6d65e79336bf310e6213f37
IsseNisse <Isakhafiu2001>
2020-02-07 12:38:33 +0100

    now the program doesn't chose same edge twice

--- src/Granne.java
+++ src/Granne.java
@@ -1,28 +1,30 @@
-import javax.swing.*;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Granne {
     public static void main(String[] args) {
         int totalDistance;
         int total = 0;
+        ArrayList<edge> chosenOnes = new ArrayList<>();
 
         graph graph = new graph();
         node currentNode;
 
         for (int i = 0; i < graph.getNodes().size(); i++) {
             int shortest = 0;
+            edge chosen = null;
             currentNode = graph.getNodes().get(i);
             ArrayList<edge> edges = currentNode.getEdges();
             for (int j = 0; j < edges.size(); j++) {
-                if (shortest == 0) {
-                    shortest = edges.get(j).getWeight();
-                } else if (edges.get(j).getWeight() < shortest) {
-                    shortest = edges.get(j).getWeight();
+                if (!chosenOnes.contains(edges.get(j))) {
+                    if (shortest == 0) {
+                        shortest = edges.get(j).getWeight();
+                    } else if (edges.get(j).getWeight() < shortest) {
+                        chosen = edges.get(j);
+                        shortest = edges.get(j).getWeight();
+                    }
                 }
             }
+            chosenOnes.add(chosen);
             total = total + shortest;
             System.out.println(total);
         }
