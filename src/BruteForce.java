7d91c13307a7da09a38632ff439864a8388fcd8d
IsseNisse <Isakhafiu2001>
2020-03-11 08:27:54 +0100

    added new forloop to get the weight of a path and check if it is te shortest

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -7,13 +7,14 @@ public class BruteForce {
         node startNode = graph.getNodes().get(0);
         ArrayList<node> nodes = graph.getNodes();
         nodes.remove(0);
-        ArrayList<ArrayList<edge>> allPaths = new ArrayList<ArrayList<edge>>();
+        ArrayList<ArrayList<edge>> allPaths = new ArrayList<>();
+        int shortestW = 0;
 
         /*startNode.heapPermutation(nodes, nodes.size(), nodes.size(), startNode);*/
         ArrayList<ArrayList<node>> perms = startNode.heapPermutation(nodes, nodes.size(), nodes.size(), startNode);
 
         for (int i = 0; i < perms.size(); i++) {
-            ArrayList<edge> path = new ArrayList<edge>();
+            ArrayList<edge> path = new ArrayList<>();
             for (int j = 0; j < perms.get(j).size(); j++) {
                 node node1 = perms.get(j).get(j);
                 node node2 = perms.get(j).get(j + 1);
@@ -21,6 +22,18 @@ public class BruteForce {
                 path.add(edge);
             }
             allPaths.add(path);
+        }
+        for (int i = 0; i < allPaths.size(); i++) {
+            int totalW = 0;
+            for (int j = 0; j < allPaths.get(j).size(); j++) {
+                int w = allPaths.get(j).get(j).getWeight();
+                totalW = totalW + w;
+                if (shortestW == 0) {
+                    shortestW = totalW;
+                } else if (totalW < shortestW) {
+                    shortestW = totalW;
+                }
+            }
         }
     }
 }
