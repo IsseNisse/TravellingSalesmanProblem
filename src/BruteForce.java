4c9c47dc58695cb819e87ffe6f5b84bf9c3a0c34
IsseNisse <Isakhafiu2001>
2020-03-11 08:16:38 +0100

    finished forloop to get edges for all paths

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -7,16 +7,20 @@ public class BruteForce {
         node startNode = graph.getNodes().get(0);
         ArrayList<node> nodes = graph.getNodes();
         nodes.remove(0);
+        ArrayList<ArrayList<edge>> allPaths = new ArrayList<ArrayList<edge>>();
 
         /*startNode.heapPermutation(nodes, nodes.size(), nodes.size(), startNode);*/
         ArrayList<ArrayList<node>> perms = startNode.heapPermutation(nodes, nodes.size(), nodes.size(), startNode);
 
         for (int i = 0; i < perms.size(); i++) {
+            ArrayList<edge> path = new ArrayList<edge>();
             for (int j = 0; j < perms.get(j).size(); j++) {
                 node node1 = perms.get(j).get(j);
                 node node2 = perms.get(j).get(j + 1);
-                System.out.println("hej");
+                edge edge = node1.getEdge(node2);
+                path.add(edge);
             }
+            allPaths.add(path);
         }
     }
 }
